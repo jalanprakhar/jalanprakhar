fc6c92df2b432b91d1520267e937d0b3ddb40d30
jalanprakhar <iit2020200@iiita.ac.in>
2022-06-27 02:59:40 +0530

    Added codev

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,29 +1,43 @@
 export const projects = [
   {
-    title: 'The DoJo',
-    description: "A platform wherein the users can collaborate on a project. They can assign task to other users, and can add comments/ mark them as completed. This project aims to help the developers in planning and building of large scale projects. It is made using React JS and Firebase",
-      image: '/images/1.png',
-      tags: ['React' , 'Firebase'],
-    source: 'https://github.com/jalanprakhar/The-DoJo',
-    visit: "https://dojo-4315d.web.app/",
+    title: "Codev",
+    description:
+      "CoDev is a social media platform wherein the users can register themselves with their email ids and provide us their github id. We can view the profiles of other amazing developers and Collaborate with them to initiate a chat and discuss any thing related to tech or otherwise😉.Wrote the complete backend of this project and contributed to the frontend.",
+    image: "/images/4.png",
+    tags: ["MongoDB", "Express", "React", "NodeJS", "TailwindCSS"],
+    source: "https://github.com/jalanprakhar/CoDev-hh2 ",
+    visit: "https://codevv.herokuapp.com/",
     id: 0,
   },
   {
-    title: 'Crypto Tracker',
-    description:"A platform where in the user can analyse the performance of various crypto-currencies and plan to buy them. The performance of various cryptos is represented using a line chart which helps the user to understand the current trend. It is made using React JS and Material UI",
-    image: '/images/2.png',
-    tags: ['React', 'Material UI'],
-    source: 'https://github.com/jalanprakhar/crypto-tracker',
-    visit: 'https://quirky-poitras-07cdb2.netlify.app/',
+    title: "The DoJo",
+    description:
+      "A platform wherein the users can collaborate on a project. They can assign task to other users, and can add comments/ mark them as completed. This project aims to help the developers in planning and building of large scale projects. It is made using React JS and Firebase",
+    image: "/images/1.png",
+    tags: ["React", "Firebase"],
+    source: "https://github.com/jalanprakhar/The-DoJo",
+    visit: "https://dojo-4315d.web.app/",
+    id: 0,
+  },
+
+  {
+    title: "Crypto Tracker",
+    description:
+      "A platform where in the user can analyse the performance of various crypto-currencies and plan to buy them. The performance of various cryptos is represented using a line chart which helps the user to understand the current trend. It is made using React JS and Material UI",
+    image: "/images/2.png",
+    tags: ["React", "Material UI"],
+    source: "https://github.com/jalanprakhar/crypto-tracker",
+    visit: "https://quirky-poitras-07cdb2.netlify.app/",
     id: 1,
   },
   {
-    title: 'Memory Magic',
-    description: "A React based game to sharpen your memory. The players are given 6 pairs of cards shuffled randomly and they have to flip cards over in pairs of 2 and match all the 6 cards in minimum number of moves",
-      image: '/images/3.png',
-      tags: ['React'],
-    source: 'https://github.com/jalanprakhar/MemoryGame',
-    visit: 'https://awesome-ptolemy-053acc.netlify.app/',
+    title: "Memory Magic",
+    description:
+      "A React based game to sharpen your memory. The players are given 6 pairs of cards shuffled randomly and they have to flip cards over in pairs of 2 and match all the 6 cards in minimum number of moves",
+    image: "/images/3.png",
+    tags: ["React"],
+    source: "https://github.com/jalanprakhar/MemoryGame",
+    visit: "https://awesome-ptolemy-053acc.netlify.app/",
     id: 2,
   },
   // {
@@ -36,4 +50,3 @@ export const projects = [
   //   id: 3,
   // },
 ];
-
