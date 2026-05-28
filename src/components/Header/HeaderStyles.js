5e2dfdbc6486ae2bfaf65f7f1ea817f3a5666063
Prakhar <iit2020200@iiita.ac.in>
2026-05-29 01:45:05 +0530

    fix(01-01): anchor responsive header actions

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -11,6 +11,7 @@ export const Container = styled.header`
 `
 
 export const NavInner = styled.nav`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: space-between;
